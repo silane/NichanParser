7c6a2f2870ef49145057018ecf342231a6b86046
silane <michiwatsuki@gmail.com>
2016-06-06 00:25:00 +0900

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+#include "NichanParser.h"
+#include <iostream>
+
+#include <ctime>
+#pragma warning(disable:4996)
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	auto thread=Nichan::ParseFromUrl("http://nhk.2ch.net/test/read.cgi/liveetv/1464944433/");
+
+	std::cout << thread.title << std::endl;
+
+	for (const auto &res : thread.res)
+	{
+		std::time_t t = std::chrono::system_clock::to_time_t(res.date);
+		std::cout << std::ctime(&t) << " : "<< res.message << std::endl;
+	}
+}
